0b499701fdcc7db4d0f492681dbec393a85317f9
Lucabiasco <luca.biasco03@gmail.com>
2026-03-04 13:46:52 +0100

    Aggiornamento file riassuntivo Sprint

--- ConwayLife/Sprint1/conway26Java/src/main/java/test/LifeTest.java
+++ ConwayLife/Sprint1/conway26Java/src/main/java/test/LifeTest.java
@@ -1,8 +1,10 @@
 package main.java.test;
 
+import static org.junit.Assert.assertFalse;
 import org.junit.After;
 import org.junit.Before;
 import main.java.conway.domain.LifeInterface;
+import org.junit.Test;
  
 /*
  * TEST PLAN
@@ -12,12 +14,21 @@ private LifeInterface lifeModel;
 
 	@Before
 	public void setup() {
-		System.out.println("GridTest | setup");	
+		System.out.println("LifeTest | setup");	
 		lifeModel = null;
 	}
 	@After
 	public void down() {
-		System.out.println("GridTest | down");
+		System.out.println("LifeTest | down");
+	}
+	
+	@Test
+	public void testNextGeneration() {
+		System.out.println("testNextGeneration ---------------------" );
+		lifeModel.resetGrids();
+		lifeModel.setCell(0, 0, true);
+		lifeModel.nextGeneration();
+		assertFalse(lifeModel.isAlive(0, 0));
 	}
 
 }
